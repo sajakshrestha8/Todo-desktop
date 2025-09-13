8b29b002e1ca742f96b13351d446b4acc198deca
sajakshrestha8 <shresthasajak123@gmail.com>
2025-09-13 09:41:50 +0545

    etablised api connection

--- todo_Client/src/App.tsx
+++ todo_Client/src/App.tsx
@@ -1,18 +1,33 @@
 import { useEffect, useState } from "react";
+import axios from "axios";
 
 function App() {
   const [message, setMessage] = useState("");
+  const [res, setRes] = useState("");
+
+  const getData = async () => {
+    try {
+      const response = await axios.get("http://localhost:8001/apiTest");
+      setRes(response.data);
+      console.log("REST API response:", response.data);
+    } catch (err) {
+      console.error("API error:", err);
+    }
+  };
 
   useEffect(() => {
+    getData();
+
     if (window.electronAPI) {
-      window.electronAPI.ping().then((res) => setMessage(res));
+      window.electronAPI.ping().then(setMessage);
     }
   }, []);
 
   return (
     <div style={{ textAlign: "center", marginTop: "50px" }}>
       <h1>Hello from React + Electron 🚀</h1>
-      <p>Backend says: {message}</p>
+      <h3>REST API says: {res}</h3>
+      <p>IPC says: {message}</p>
     </div>
   );
 }
--- todo_Server/index.js
+++ todo_Server/index.js
@@ -1,24 +1,33 @@
 const { app, BrowserWindow, ipcMain } = require("electron");
 const path = require("path");
+const express = require("express");
+
+const appApi = express();
+const cors = require("cors");
+
+appApi.use(cors());
+
+appApi.get("/apiTest", (req, res) => res.send("hello testing from the server"));
+appApi.listen(8001, () => console.log("Server running on 8001"));
 
 function createWindow() {
-    const window = new BrowserWindow({
-      width: 1000,
-      height: 700,
-      webPreferences: {
-        preload: path.join(__dirname, "preload.js"),
-      }
-    });
+  const window = new BrowserWindow({
+    width: 1000,
+    height: 700,
+    webPreferences: {
+      preload: path.join(__dirname, "preload.js"),
+      contextIsolation: true,
+      nodeIntegration: false,
+    },
+  });
 
-    window.loadURL("http://localhost:5173");
-};
+  window.loadURL("http://localhost:5173");
+}
 
-ipcMain.handle("ping", async () => {
-    return "pong from Server";
-});
+ipcMain.handle("ping", async () => "pong from Server");
 
 app.whenReady().then(createWindow);
 
 app.on("window-all-closed", () => {
-    if(process.platform !== 'darwin') app.quit(); 
+  if (process.platform !== "darwin") app.quit();
 });
